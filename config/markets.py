41ce657f2407093ae21b92bb40391c3f1c3306ae
sora <moussa.sissoko6@etu.univ-lorraine.fr>
2026-06-24 17:31:10 +0000

    feat: Add markets configuration

---
+++ config/markets.py
@@ -0,0 +1,15 @@
+
+MARKETS_UNIVERSE = {
+    "CAC40": [
+        "AI.PA", "AIR.PA", "ALO.PA", "MT.AS", "ATO.PA", "CS.PA", "BNP.PA",
+        "EN.PA", "CAP.PA", "CA.PA", "DSY.PA", "EL.PA", "ENGI.PA", "ERF.PA",
+        "RMS.PA", "KER.PA", "OR.PA", "LR.PA", "MC.PA", "ML.PA", "ORA.PA",
+        "RI.PA", "PUB.PA", "RNO.PA", "SAF.PA", "SGO.PA", "SAN.PA", "SU.PA",
+        "GLE.PA", "STLAP.PA", "STMPA.PA", "TEP.PA", "HO.PA", "TTE.PA",
+        "URW.PA", "VIE.PA", "DG.PA", "VIV.PA", "WLN.PA", "FR.PA"
+    ],
+    #    "BRVM": [ ]
+}
+
+# Global params
+HISTORY_YEARS = 10
